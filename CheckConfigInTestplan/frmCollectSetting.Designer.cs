30e8829d5b6a5f354ad92a3f292ca1fc2c791b07
Edward <edward_song@yeah.net>
2018-07-06 10:24:58 +0800

    add setting

---
+++ CheckConfigInTestplan/frmCollectSetting.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CheckConfigInTestplan
+{
+    partial class frmCollectSetting
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbSetting = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblfolder = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grbSetting.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbSetting
+            // 
+            this.grbSetting.Controls.Add(this.label1);
+            this.grbSetting.Controls.Add(this.textBox3);
+            this.grbSetting.Controls.Add(this.label2);
+            this.grbSetting.Controls.Add(this.btnTest);
+            this.grbSetting.Controls.Add(this.textBox2);
+            this.grbSetting.Location = new System.Drawing.Point(12, 12);
+            this.grbSetting.Name = "grbSetting";
+            this.grbSetting.Size = new System.Drawing.Size(270, 88);
+            this.grbSetting.TabIndex = 0;
+            this.grbSetting.TabStop = false;
+            this.grbSetting.Text = "DataBaseSetting";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "DataBaseIP";
+            // 
+            // lblfolder
+            // 
+            this.lblfolder.AutoSize = true;
+            this.lblfolder.Location = new System.Drawing.Point(12, 114);
+            this.lblfolder.Name = "lblfolder";
+            this.lblfolder.Size = new System.Drawing.Size(86, 14);
+            this.lblfolder.TabIndex = 4;
+            this.lblfolder.Text = "ProgramFolder";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(184, 30);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 32);
+            this.btnTest.TabIndex = 3;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(85, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "127.0.0.1";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 52);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(85, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 14);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "DataBase";
+            // 
+            // frmCollectSetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 147);
+            this.Controls.Add(this.grbSetting);
+            this.Controls.Add(this.lblfolder);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmCollectSetting";
+            this.Text = "Setting";
+            this.grbSetting.ResumeLayout(false);
+            this.grbSetting.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbSetting;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblfolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
--- CheckConfigInTestplan/frmCollectSetting.cs
+++ CheckConfigInTestplan/frmCollectSetting.cs
@@ -8,9 +8,9 @@ using System.Windows.Forms;
 
 namespace CheckConfigInTestplan
 {
-    public partial class Form1 : Form
+    public partial class frmCollectSetting : Form
     {
-        public Form1()
+        public frmCollectSetting()
         {
             InitializeComponent();
         }
